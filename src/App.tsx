84a14af86d597788982fc9384cc67b388fb7091e
Vilmos Ioo <cool.villi@gmail.com>
2020-12-26 14:52:13 +0000

    cubes

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useRef } from 'react';
 import './App.css';
+import { useFrame, MeshProps } from 'react-three-fiber';
+import * as Three from 'three';
 
-function App() {
+function App(props: MeshProps) {
+  const mesh = useRef<Three.Mesh>();
+
+  useFrame(() => {
+    if (mesh.current) mesh.current.rotation.x = mesh.current.rotation.y += 0.01;
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <mesh ref={mesh} {...props}>
+      <boxBufferGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color={'orange'} />
+    </mesh>
   );
 }
 
