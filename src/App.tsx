d3c3d24d16ab67d9154f970a8f10c0699252f5c4
vilmosioo <vilmosioo@users.noreply.github.com>
2020-12-29 14:37:34 +0000

    Deploying to gh-pages from  @ 453afb596c9af07d5482bfe3166e240a652a7fd6 🚀

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import * as React from 'react'
+import './App.css'
+import { useFrame, MeshProps } from 'react-three-fiber'
+import * as Three from 'three'
+
+function App (props: MeshProps) {
+  const mesh = React.useRef<Three.Mesh>()
+
+  useFrame(() => {
+    if (mesh.current) mesh.current.rotation.x = mesh.current.rotation.y += 0.01
+  })
+
+  return (
+    <mesh ref={mesh} {...props}>
+      <boxBufferGeometry args={[2, 2, 2]} />
+      <meshStandardMaterial color='blue' />
+    </mesh>
+  )
+}
+
+export default App
