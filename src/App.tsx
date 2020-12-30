ccda1d7b77014e884b9301f886abf76e9f972673
Vilmos Ioo <vilmosioo@users.noreply.github.com>
2020-12-30 00:30:56 +0000

    camera (#16)

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,10 @@
 import * as React from 'react'
 import './App.css'
-import { useFrame, MeshProps } from 'react-three-fiber'
+import { MeshProps } from 'react-three-fiber'
 import * as Three from 'three'
 
 function App (props: MeshProps) {
   const mesh = React.useRef<Three.Mesh>()
-
-  useFrame(() => {
-    if (mesh.current) mesh.current.rotation.x = mesh.current.rotation.y += 0.01
-  })
 
   return (
     <mesh ref={mesh} {...props}>
